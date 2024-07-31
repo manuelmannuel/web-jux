52d9c89b788c7011737fdb33f819d3440ae3023a
devtimx <devtimx@inspiron15.devti.dev>
2024-07-30 19:50:12 -0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import './App.css'
+function App() {
+
+  return (
+    <>
+      <header className='text-head'>
+        <h1>Mi Juxtlahuaca</h1>
+        <h3>Sitio en construcción</h3>
+        <p>Sugerencias: <a href="mailto:devti.mx@gmail.com? Subject=Sugerencias Mi Juxtlahuaca">Aquí</a></p>
+      </header>
+      <section>
+        <img src="https://img.freepik.com/foto-gratis/concepto-renovacion-proyecto-diseno-construccion_53876-120336.jpg?t=st=1722212623~exp=1722216223~hmac=f60509b5bceb57789f4fd073a1655354f03f3421f80891fb953db8d25c3d698e&w=1380" alt="" />
+      </section>
+    </>
+  )
+}
+
+export default App
